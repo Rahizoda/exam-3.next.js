f5ae00fd227e92efbaa57e3d59a50f10684f4f1a
Muhammadkarim <sharifarahimi61@gmail.com>
2025-09-30 10:28:02 +0500

    .

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -9,7 +9,7 @@ const page = () => {
     <div className='pt-[100px] bg-accent dark:bg-[#050524]'>
 
       <div className='flex flex-col bg-[#caebfd] dark:bg-[#070727] justify-center  p-[50px_0px] items-center gap-10'>
-        <h1 className='text-blue-700 text-[70px] font-extrabold' >Aboute Us</h1>
+        <h1 className='text-blue-700 text-[70px] font-extrabold' >Contact</h1>
         <p ><span onClick={() => router.push("/")} className='cursor-pointer hover:underline'>Home  </span>  <EastIcon /> <span className='cursor-pointer underline'>Contact</span></p>
       </div>
 
